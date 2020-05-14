8b7ec72f2519ccff6aa7bddba5c741a498b3d918
modernfuthark <timothys1738@gmail.com>
2020-05-13 20:43:48 -0400

    Added files for task 4 to directory

--- add.c
+++ add.c
@@ -1,13 +1,27 @@
 #include "monty.h"
 
 /**
- * 
- * 
- * 
+ * add - Add the top 2 nodes of the Stack, and removes the head
+ * @STACK: The stack
+ * @lineno: Line number (for errors)
+ * Return: Nothing, function is void.
  */
 
 void add(stack_t **STACK, unsigned int lineno)
 {
-	(void)STACK;
-	(void)lineno;
+	stack_t *temp;
+
+	if (!STACK || !*STACK || !(*STACK)->next)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", lineno);
+		gfree(STACK);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *STACK;
+
+	temp->next->n = temp->n + temp->next->n;
+	*STACK = temp->next;
+
+	free(temp);
 }
