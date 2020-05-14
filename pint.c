23ad36a8e9ade5e134c95ab51e77ab6b487fd437
modernfuthark <timothys1738@gmail.com>
2020-05-13 20:21:09 -0400

    Added files to task 1

--- pint.c
+++ pint.c
@@ -1,13 +1,18 @@
 #include "monty.h"
 
 /**
- * 
- * 
- * 
+ * pint - Print the top of the stack
+ * @STACK: The stack
+ * @lineno: Line number
  */
 
 void pint(stack_t **STACK, unsigned int lineno)
 {
-	(void)STACK;
-	(void)lineno;
+	if (!STACK || !*STACK)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", lineno);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*STACK)->n);
 }
