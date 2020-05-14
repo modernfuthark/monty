0df05c31a0998aa5121d8d6d1a345f2b8e6a77df
modernfuthark <timothys1738@gmail.com>
2020-05-13 20:26:49 -0400

    Added file for task 2

--- pop.c
+++ pop.c
@@ -1,13 +1,27 @@
 #include "monty.h"
 
 /**
- * 
- * 
- * 
+ * pop - Pop the top node of STACK
+ * @STACK: The stack
+ * @lineno: Line number (for errors)
+ * Return: Nothing, function is void.
  */
 
 void pop(stack_t **STACK, unsigned int lineno)
 {
-	(void)STACK;
-	(void)lineno;
+	stack_t *temp;
+
+	if (!STACK || !*STACK)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", lineno);
+		gfree(STACK);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*STACK)->next;
+	free(*STACK);
+	*STACK = temp;
+	if (!*STACK)
+		return;
+	(*STACK)->prev = NULL;
 }
