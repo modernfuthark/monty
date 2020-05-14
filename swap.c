cfccd41e8a16e599b21ebfb8db884a13fcfe72b8
modernfuthark <timothys1738@gmail.com>
2020-05-13 20:37:14 -0400

    Added files for task 3

--- swap.c
+++ swap.c
@@ -1,13 +1,27 @@
 #include "monty.h"
 
 /**
- * 
- * 
- * 
+ * swap - Swap the top 2 nodes of the Stack
+ * @STACK: The stack
+ * @lineno: Line number (for errors
+ * Return: Nothing, function is void.
  */
 
 void swap(stack_t **STACK, unsigned int lineno)
 {
-	(void)STACK;
-	(void)lineno;
+	stack_t *temp;
+	int hold;
+
+	if (!STACK || !*STACK || !(*STACK)->next)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", lineno);
+		gfree(STACK);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *STACK;
+	hold = temp->n;
+
+	temp->n = temp->next->n;
+	temp->next->n = hold;
 }
