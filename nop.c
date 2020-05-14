e69ac49a92bc9621d22ecf8c1bee9a8e0788553f
modernfuthark <timothys1738@gmail.com>
2020-05-13 20:45:04 -0400

    Betty formatting

--- nop.c
+++ nop.c
@@ -1,9 +1,10 @@
 #include "monty.h"
 
 /**
- * 
- * 
- * 
+ * nop - Do jack all
+ * @STACK: The stack
+ * @lineno: Line number
+ * Return: Nothing, function is void.
  */
 
 void nop(stack_t **STACK, unsigned int lineno)
